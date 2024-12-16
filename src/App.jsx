b220ff29e1f87e344e070e6ce5403ecb528c1678
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-12-16 21:01:15 +0530

    added toggleCategory

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import CategoryScreen from "./components/CategoryScreen";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [category, setCategory] = useState(false);
+
+  const toggleCategory = () => {
+    setCategory(!category);
+  };
+
   return (
-    <div className="wrapper">
+    <div className={`wrapper ${category ? "show-category" : ""}`}>
       <div className="screen-backdrop"></div>
-      <HomeScreen />
+      <HomeScreen toggleCategory={toggleCategory} />
       <CategoryScreen />
       <div className="add-task-btn">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-6 h-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M12 4.5v15m7.5-7.5h-15"
           />
         </svg>
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import boy from "../assets/images/boy.png";
 
-function HomeScreen() {
+function HomeScreen({ toggleCategory }) {
   return (
     <div className="home-screen screen">
       <div className="head-wrapper">
@@ -9,13 +10,13 @@ function HomeScreen() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
             />
           </svg>
@@ -29,15 +30,15 @@ function HomeScreen() {
           </div>
           <div className="img">
             <div className="backdrop"></div>
-            <img src="images/boy.png" alt="" />
+            <img src={boy} alt="" />
           </div>
         </div>
       </div>
       <div className="categories-wrapper">
-        <div className="categories">
+        <div className="categories" onClick={toggleCategory}>
           <div className="category">
             <div className="left">
-              <img src="images/boy.png" alt="sun" />
+              <img src={boy} alt="sun" />
               <div className="content">
                 <h1>Personal</h1>
                 <p>5 Tasks</p>
@@ -49,13 +50,13 @@ function HomeScreen() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
                   />
                 </svg>
