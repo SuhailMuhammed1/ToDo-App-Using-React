6bd25b390adde368c5e9b7fa01e6b59cc8f0e1b8
SuhailMuhammed1 <suhailjr111@gmail.com>
2025-03-03 16:07:38 +0530

    changes.

--- src/components/LayoutRoutes.jsx
+++ src/components/LayoutRoutes.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeScreen from "./HomeScreen";
-import CategoryScreen from "./CategoryScreen";
-import Home from "./Home";
-
 function LayoutRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/category/:categoryName" element={<CategoryScreen />} />
+        <Route />
       </Routes>
     </Router>
   );
